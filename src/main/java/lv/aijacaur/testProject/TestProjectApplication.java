1914ea31689e93f0bc6f3461a6890c5d530a30e9
Aleksandrs Gusevs <aleksandrs.gusevs@autentica.lv>
2017-09-26 14:55:17 +0300

    Initial commit

---
+++ src/main/java/lv/aijacaur/testProject/TestProjectApplication.java
@@ -0,0 +1,41 @@
+package lv.aijacaur.testProject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
+public class TestProjectApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(TestProjectApplication.class);
+
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(TestProjectApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
+    }
+}
---
+++ src/main/java/lv/aijacaur/testProject/config/WebConfigurer.java
@@ -0,0 +1,63 @@
+package lv.aijacaur.testProject.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.MimeMappings;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.inject.Inject;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.Arrays;
+
+/**
+ * Configuration of web application with Servlet 3.0 APIs.
+ */
+@Configuration
+public class WebConfigurer implements ServletContextInitializer, EmbeddedServletContainerCustomizer {
+
+    private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
+
+    @Inject
+    private Environment env;
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
+
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+        initH2Console(servletContext);
+
+        log.info("Web application fully configured");
+    }
+
+    /**
+     * Set up Mime types.
+     */
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
+        container.setMimeMappings(mappings);
+    }
+
+    /**
+     * Initializes H2 console.
+     */
+    private void initH2Console(ServletContext servletContext) {
+        log.debug("Initialize H2 console");
+        ServletRegistration.Dynamic h2ConsoleServlet = servletContext.addServlet("H2Console", new org.h2.server.web.WebServlet());
+        h2ConsoleServlet.addMapping("/h2-console/*");
+        h2ConsoleServlet.setInitParameter("-properties", "src/main/resources/");
+        h2ConsoleServlet.setLoadOnStartup(1);
+    }
+}
